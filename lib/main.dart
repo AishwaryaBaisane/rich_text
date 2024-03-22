996b08d0d0f27c3a57dd6f68f9e15a7f8baa6ccb
RNW <baisanehira@gmail.com>
2024-03-22 17:00:03 +0530

    22-03-2024

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,16 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: true,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: ''),
-
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-
 
   final String title;
 
@@ -40,41 +36,62 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-
         centerTitle: true,
-        title: Text('RichText Example',style: TextStyle(color :Colors.white,fontWeight: FontWeight.bold,fontSize: 30),),
-      ),
-      body:Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Text.rich(TextSpan(children:[
-
-          TextSpan(text: 'Single  ',style: TextStyle(color :Colors.blue,fontWeight: FontWeight.bold,fontSize: 35,height: -0.101),),
-          TextSpan(text: ' Line ',style: TextStyle(backgroundColor: Colors.teal,fontWeight: FontWeight.bold,fontSize: 25,color:Colors.white),),
-          TextSpan(text: ' Multiple',style: TextStyle(color :Colors.amber,fontSize: 30,letterSpacing: 3),),
-          TextSpan(text: ' Style',style: TextStyle(color :Colors.pink,fontSize: 30,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic),),
-
-
-        ],),
-
-
-
-
-
+        title: const Text(
+          'RichText Example',
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
         ),
       ),
-   // / This trailing comma makes auto-formatting nicer for build methods.
+      body: const Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: 'Single  ',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35,
+                    height: -0.101),
+              ),
+              TextSpan(
+                text: ' Line ',
+                style: TextStyle(
+                    backgroundColor: Colors.teal,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Colors.white),
+              ),
+              TextSpan(
+                text: ' Multiple',
+                style: TextStyle(
+                    color: Colors.amber, fontSize: 30, letterSpacing: 3),
+              ),
+              TextSpan(
+                text: ' Style',
+                style: TextStyle(
+                    color: Colors.pink,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic),
+              ),
+            ],
+          ),
+        ),
+      ),
+      // / This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
